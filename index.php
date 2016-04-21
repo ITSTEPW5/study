c05413757994fc1b5d3369945eed1d29936ff70e
Tserkovna <lilichkatserkovna@gmail.com>
2016-04-21 21:00:49 +0300

    Switch to php

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php include("layout/header.php"); ?>
+    <div class="main">
+		<h1>Hello, world!</h1>
+	</div>
+<?php include("layout/footer.php"); ?>
---
+++ layout/footer.php
@@ -0,0 +1,11 @@
+<div class="footer">   
+<!-- Vendor scripts -->
+		<script src="assets/vendor/vendor.js"></script>
+	
+	
+		<!-- Aplication scripts -->
+		<script src="assets/js/script.js"></script>
+</div>
+
+	</body>
+</html>
